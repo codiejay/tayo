275168c41d948aeb585f79d739e2f77c99ced842
James Akpan <arfitcode@gmail.com>
2019-08-07 20:49:01 +0100

    new background

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ inquiry.addEventListener("click", (e) => {
 
     e.preventDefault();
     $(".form").show(200);
-    $(".body").css({background: "#fff"})
+    $(".body").css({background: `linear-gradient(to left, #4747476c  40%, rgba(240, 240, 240, 0.92) ) , url("../newHead-min.jpg")`, backgroundPosition: "center", backgroundSize: "cover"})
 })
+
+
+
 
 cancelBttn.on("click", closeForm)
 
